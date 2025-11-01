1b7261edbfcb4c58f51fcf7f16918099b4716f3d
marvelve <marysela.velasco@gmail.com>
2025-11-01 14:29:43 -0500

    actualizacion2 para pruebas postman2

--- src/main/java/com/interivalle/Modelo/Usuario.java
+++ src/main/java/com/interivalle/Modelo/Usuario.java
@@ -69,6 +69,7 @@ public class Usuario {
         this.contrasenaUsuario = contrasenaUsuario;
         this.celularUsuario = celularUsuario;
         this.nombreProyectoUsuario = nombreProyectoUsuario;
+        this.idRol=idRol;
     }
 
     //Getters y setters   
